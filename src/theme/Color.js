34812a91829dd4963f6e7e7e724ef0e96aa60676
Rahul Nakate <rahul.nakate@synerzip.com>
2020-07-07 00:02:06 +0530

    First Commit

---
+++ src/theme/Color.js
@@ -0,0 +1,16 @@
+export default {
+  appLightBackground: '#F4F6FB',
+  appBlue: '#313381',
+  borderGrey: '#BCBCBC',
+  greyText: 'rgba(151,152,153,1)',
+  appOrange: '#F48C05',
+  black: '#000000',
+
+  appGrey: '#f5f5f5',
+  blueColor: '#463d8f',
+  dullBlue: '#6E71A3',
+  buttonBlue: '#463D8F',
+  appGreen: '#66C186',
+  appRed: '#EC524A',
+  headerBlue: '#31357C',
+};
